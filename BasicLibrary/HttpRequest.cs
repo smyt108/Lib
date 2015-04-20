210a439c72b2c82e5cafc0559082e485a38b37a6
smyt108 <smyt108@163.com>
2015-04-20 12:07:55 +0800

    Add http request post method.

---
+++ BasicLibrary/HttpRequest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace BasicLibrary
+{
+    public class HttpRequest
+    {
+        public string WebPostRequest(string url, string postData)
+        {
+            HttpWebRequest webRequest = null;
+            StreamWriter requestWriter = null;
+            string responseData = "";
+
+            webRequest = System.Net.WebRequest.Create(url) as HttpWebRequest;
+            webRequest.Method = "POST";
+            webRequest.ServicePoint.Expect100Continue = false;
+            //webRequest.UserAgent  = "Identify your application please.";
+            //webRequest.Timeout = 20000;
+
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+            //webRequest.ContentType = "multipart/form-data";
+
+            //POST the data.
+            requestWriter = new StreamWriter(webRequest.GetRequestStream());
+            try
+            {
+                requestWriter.Write(postData);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                requestWriter.Close();
+                requestWriter = null;
+            }
+
+            responseData = WebResponseGet(webRequest);
+
+            webRequest = null;
+
+            return responseData;
+        }
+
+        public string WebResponseGet(HttpWebRequest webRequest)
+        {
+            StreamReader responseReader = null;
+            string responseData = "";
+
+            try
+            {
+                responseReader = new StreamReader(webRequest.GetResponse().GetResponseStream());
+                responseData = responseReader.ReadToEnd();
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                webRequest.GetResponse().GetResponseStream().Close();
+                responseReader.Close();
+                responseReader = null;
+            }
+
+            return responseData;
+        }
+    }
+}
