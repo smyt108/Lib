e7f778664d02db78b806dc2dfc2d8cf13610ee14
Jesse yang <smyt108@163.com>
2016-02-19 21:09:28 +0800

    for thea

---
+++ Libs/theaTabControl/DevExpress/View/CustomTabControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using DevExpress.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DevExpress.View
+{
+    /// <summary>
+    /// Interaction logic for CustomTabControl.xaml
+    /// </summary>
+    public partial class CustomTabControl : UserControl
+    {
+        public CustomTabControl()
+        {
+            InitializeComponent();
+        }
+
+        public ObservableCollection<CustomTabControlModel> ListTabItemSource
+        {
+            get { return (ObservableCollection<CustomTabControlModel>)GetValue(ListTabItemSourceProperty); }
+            set { SetValue(ListTabItemSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty ListTabItemSourceProperty =
+            DependencyProperty.Register("ListTabItemSource", typeof(ObservableCollection<CustomTabControlModel>), typeof(CustomTabControl));
+
+    }
+}
---
+++ Libs/theaTabControl/DevExpress/View/QueryPanelView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using DevExpress.ViewModel;
+using System.Windows.Controls;
+
+namespace DevExpress
+{
+    /// <summary>
+    /// Interaction logic for QueryPanelView.xaml
+    /// </summary>
+    public partial class QueryPanelView : UserControl
+    {
+        public QueryPanelView()
+        {
+            InitializeComponent();
+            QueryPanelViewModel viewModel = new QueryPanelViewModel();
+            this.DataContext = viewModel;
+        }    
+    }
+}
