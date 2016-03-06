eeffa4054eeb1b0acf58b92c33cb21230fb91572
smyt108 <smyt108@163.com>
2016-03-07 00:08:09 +0800

    Behavior&CommandParameter

---
+++ Libs/BehaviorAndCommandParameterSample/BehaviorAndCommandParameterSample/ChangeDataGridBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace BehaviorAndCommandParameterSample
+{
+    public static class ChangeDataGridBackgroundBehavior
+    {
+        public static readonly DependencyProperty BackgroundProperty =
+            DependencyProperty.RegisterAttached("Background", typeof(string), typeof(DataGrid), new FrameworkPropertyMetadata(DataGridBackgroundChanged));
+
+        public static string GetBackground(DependencyObject element)
+        {
+            return (string)element.GetValue(BackgroundProperty);
+        }
+
+        public static void SetBackground(DependencyObject element, string value)
+        {
+            element.SetValue(BackgroundProperty, value);
+        }
+
+        private static void DataGridBackgroundChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            DataGrid dg = d as DataGrid;
+            dg.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(e.NewValue.ToString()));
+        }
+    }
+}
---
+++ Libs/BehaviorAndCommandParameterSample/BehaviorAndCommandParameterSample/SampleViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Practices.Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BehaviorAndCommandParameterSample
+{
+    public class SampleViewModel : INotifyPropertyChanged
+    {
+        #region Constructer
+
+        public SampleViewModel()
+        {
+            ButtonCommand = new DelegateCommand<object>(Commands.ChangeBackgroundCommand);
+        }
+
+        #endregion
+
+        public ICommand ButtonCommand { get; set; }
+
+        #region Implement INotify
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged!=null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
